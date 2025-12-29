4e30c820569f1eeb59dcd8a8027a461d2dbff8b0
Oluwatosin Olufowobi <fowobideveloper@gmail.com>
2025-12-29 18:00:41 +0100

    Added styles

--- src/components/PlayerDetails.tsx
+++ src/components/PlayerDetails.tsx
@@ -28,27 +28,48 @@ const PlayerDetails = () => {
       }, [dispatch]);
 
   return (
-    <main className="box">
+    <main className="box player-details">
       <h2>Player Record</h2>
         <div className="img-container">
             <img src={import.meta.env.VITE_BASE_URL + playerDetailsResponse.body.passportPhotoUrl} width={200} />
         </div>
-        <div>
-          <p>Firstname: {playerDetailsResponse.body.firstname}</p>
-          <p>Middlename: {playerDetailsResponse.body.middlename}</p>
-          <p>Lastname: {playerDetailsResponse.body.lastname}</p>
-          <p>Date of Birth: {playerDetailsResponse.body.dob}</p>
-          <p>Height: {playerDetailsResponse.body.playerHeight}</p>
-          <p>Weight: {playerDetailsResponse.body.playerWeight}</p>
-          <p>State of Origin: {playerDetailsResponse.body.originState}</p>
-          <p>Nationality: {playerDetailsResponse.body.nationality}</p>
-          <p>Phone: {playerDetailsResponse.body.playerPhone}</p>
-          <p>Address: {playerDetailsResponse.body.playerAddress}</p>
-          <p>Jersey Number: {playerDetailsResponse.body.squadNumber}</p>
-          <p>Address: {playerDetailsResponse.body.playerAddress}</p>
-          <p>Special Health Concerns: {playerDetailsResponse.body.healthConcernDescription ? playerDetailsResponse.body.healthConcernDescription : "None"}</p>
-          <p>Status: {playerDetailsResponse.body.membershipStatus}</p>
-          <p>Parent Address: {playerDetailsResponse.body.parentAddress}</p>
+        <div className="player-data">
+          <p>Firstname: <span>{playerDetailsResponse.body.firstname}</span></p>
+          <p>Middlename: <span>{playerDetailsResponse.body.middlename}</span></p>
+          <p>Lastname: <span>{playerDetailsResponse.body.lastname}</span></p>
+          <p>Date of Birth: <span>{playerDetailsResponse.body.dob}</span></p>
+          <p>Height: <span>{playerDetailsResponse.body.playerHeight}</span></p>
+          <p>Weight: <span>{playerDetailsResponse.body.playerWeight}</span></p>
+          <p>State of Origin: <span>{playerDetailsResponse.body.originState}</span></p>
+          <p>Nationality: <span>{playerDetailsResponse.body.nationality}</span></p>
+          <p>Phone: <span>{playerDetailsResponse.body.playerPhone}</span></p>
+          <p>Address: <span>{playerDetailsResponse.body.playerAddress}</span></p>
+          <p>Jersey Number: <span>{playerDetailsResponse.body.squadNumber}</span></p>
+          <p>Address: <span>{playerDetailsResponse.body.playerAddress}</span></p>
+          <p>Special Health Concerns: <span>{playerDetailsResponse.body.healthConcernDescription ? playerDetailsResponse.body.healthConcernDescription : "None"}</span></p>
+          <p>Status: <span>{playerDetailsResponse.body.membershipStatus}</span></p>
+          
+          <b>Parent Information</b>
+          <p>Parent Title: <span>{playerDetailsResponse.body.parentTitle}</span></p>
+          <p>Parent Firstname: <span>{playerDetailsResponse.body.parentFirstname}</span></p>
+          <p>Parent Middlename: <span>{playerDetailsResponse.body.parentMiddlename}</span></p>
+          <p>Parent Lastname: <span>{playerDetailsResponse.body.parentLastname}</span></p>
+          <p>Parent Address: <span>{playerDetailsResponse.body.parentAddress}</span></p>
+          <p>Parent Phone Number: <span>{playerDetailsResponse.body.parentPhone}</span></p>
+          <p>Parent Address: <span>{playerDetailsResponse.body.parentEmail}</span></p>
+          <p>Parent Address: <span>{playerDetailsResponse.body.parentAddress}</span></p>
+
+          {
+            playerDetailsResponse.body.sponsorFirstname && (
+              <>
+                <p>Sponsor Title: <span>{playerDetailsResponse.body.sponsorTitle}</span></p>
+                <p>Sponsor Firstname: <span>{playerDetailsResponse.body.sponsorFirstname}</span></p>
+                <p>Sponsor Lastname: <span>{playerDetailsResponse.body.sponsorLastname}</span></p>
+                <p>Sponsor Occupation: <span>{playerDetailsResponse.body.sponsorOccupation}</span></p>
+                <p>Sponsor Email: <span>{playerDetailsResponse.body.sponsorEmail}</span></p>
+              </>
+            )
+          }
         </div>
       <ToastContainer position="top-right" autoClose={3000} />
     </main>
